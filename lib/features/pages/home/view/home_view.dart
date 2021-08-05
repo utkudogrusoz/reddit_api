24d5c2b9df77ec4990d42cb5ac638be4367f867a
Utku Doğrusöz <utku.dogrusoz@gmail.com>
2021-08-05 16:29:27 +0300

    first commit

--- lib/features/pages/home/view/home_view.dart
+++ lib/features/pages/home/view/home_view.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reddit_api/core/exception/widget_not_found_exception.dart';
-
 import 'package:reddit_api/features/pages/home/viewmodel/home_cubit_state.dart';
 import 'sub_views/home_list_item_state.dart';
 import 'package:reddit_api/core/extensions/context_extension.dart';
-
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -23,22 +21,26 @@ class HomeView extends StatelessWidget {
           },
           builder: (context, state) {
             return Scaffold(
-              appBar: AppBar(
-                title: Container(
-                  width: context.width * 0.30,
-                  height: context.height * 0.40,
-                  decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/logo/Reddit_logo.png'))),
-                ),
-              ),
+              appBar: homeAppBar(context),
               body: buildBody(state, context),
             );
           },
         ));
   }
 
+  AppBar homeAppBar(BuildContext context) {
+    return AppBar(
+      title: Container(
+        width: context.width * 0.30,
+        height: context.height * 0.40,
+        decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/logo/Reddit_logo.png'))),
+      ),
+    );
+  }
+
   Widget buildBody(HomeState state, BuildContext context) {
     if (state is HomeInitial) {
-      return Text('Products Initial State');
+      return Text('Home Initial State');
     } else if (state is HomeLoadingState) {
       return Center(child: CircularProgressIndicator());
     } else if (state is HomeListItemState) {
--- lib/features/pages/home/view/sub_views/home_list_item_state.dart
+++ lib/features/pages/home/view/sub_views/home_list_item_state.dart
@@ -1,3 +1,4 @@
+import 'package:reddit_api/features/model/response_model/reddit_model.dart';
 import 'package:reddit_api/features/pages/home/viewmodel/home_cubit_state.dart';
 import 'package:flutter/material.dart';
 import 'package:reddit_api/core/extensions/context_extension.dart';
@@ -7,37 +8,33 @@ extension HomeListItemWidget on HomeListItemState {
     return ListView.builder(
         itemCount: posts.length,
         itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(posts[index].data!.title!),
-              leading: _nullCheckImages(context,posts[index].data!.thumbnail)
-            ),
-          );
+          return listCard(posts, index, context);
         });
   }
+}
 
-  Widget _nullCheckImages(BuildContext context, String? url) {
-    if (url != null) {
-      return Container(
-        width: context.width * 0.30,
-        height: context.height * 0.40,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(
-             url,
-            ),
-            fit: BoxFit.cover,
-          ),
+Card listCard(List<Child> posts, int index, BuildContext context) {
+  return Card(
+    child: ListTile(
+      title: cardTitle(posts, index),
+      leading: cardLeadingImage(posts, index, context),
+    ),
+  );
+}
+
+Text cardTitle(List<Child> posts, int index) => Text(posts[index].data!.title!);
+
+Container cardLeadingImage(List<Child> posts, int index, BuildContext context) {
+  return Container(
+    width: context.width * 0.30,
+    height: context.height * 0.40,
+    decoration: BoxDecoration(
+      image: DecorationImage(
+        image: NetworkImage(
+          posts[index].data!.thumbnail!,
         ),
-      );
-    } else {
-      return Container(
-        width: context.width * 0.30,
-        height: context.height * 0.40,
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage('assets/logo/Reddit_logo.png')),
-        ),
-      );
-    }
-  }
+        fit: BoxFit.cover,
+      ),
+    ),
+  );
 }
